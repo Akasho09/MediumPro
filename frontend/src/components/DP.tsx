6e71aeb16333fe1d5321b0aeecaa07bb85a253cf
Akasho09 <aakashbwd@gmail.com>
2025-01-23 15:39:31 +0530

    s

--- frontend/src/components/DP.tsx
+++ frontend/src/components/DP.tsx
@@ -1,7 +1,6 @@
 
-export default function DP( username  : string ){
+export default function DP( ){
 
-    let a = username.split(' ');
-    return (<div className="rounded-full bg-green-400 h-10 w-10 flex justify-center items-center">{a[0]}{a[1]}</div>
+    return (<div className="rounded-full bg-green-400 h-10 w-10 flex justify-center items-center">AM</div>
 )
 }
