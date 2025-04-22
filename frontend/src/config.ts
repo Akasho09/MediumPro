02cd1cfa2b5658754ccffb3163bd0aac4df1b301
Akasho09 <aakashbwd@gmail.com>
2025-04-23 00:45:16 +0530

    cssplus

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL= "http://localhost:8787" 
-// export const BACKEND_URL= "https://backend.aakashbwd.workers.dev" 
+// export const BACKEND_URL= "http://localhost:8787" 
+export const BACKEND_URL= "https://backend2.aakashbwd.workers.dev" 
